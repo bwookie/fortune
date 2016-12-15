6221d3e55de76443827d0862f823f166774adad0
Meagan Gates <Meagan.Gates@Cerner.com>
2016-12-15 15:11:20 -0600

    Initial commit

---
+++ spec/fortune_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe Fortune do
+  it "has a version number" do
+    expect(Fortune::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
